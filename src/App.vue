082883b7a512f9d22907d13b1840f9ac0dde1763
hungnv1-seventh-pitch <hung.nv1@seventh-pitch.com>
2024-06-11 14:40:09 +0700

    build ci cd

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">Aboutaaaaaaaaaaaaa</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
